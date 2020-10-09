f813103feb36731753f7f8c9c6b010d7649a0cd0
Robert Rozas Navarro <rorozasn@microsoft.com>
2020-10-08 21:11:55 -0300

    app lista

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetCoreUploadDemo.Models;
+
+namespace NetCoreUploadDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Images(string file="")
+        {
+            if (file.Length > 0)
+            {
+                System.IO.File.Delete(@"wwwroot\images\"+file);
+            }
+
+            string[] fileEntries = Directory.GetFiles(@"wwwroot\images").Select(file => Path.GetFileName(file)).ToArray(); ;
+            return View(fileEntries);
+        }
+
+        public async Task<JsonResult> Upload(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var fileName = Path.GetFileName(formFile.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", fileName);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return new JsonResult(new { count = files.Count, size });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
